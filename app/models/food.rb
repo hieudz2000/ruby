37e2fb037a15185ca3ed213948386692912b00aa
vananhbui41 <vananhbui41@gmail.com>
2021-12-12 09:19:30 -0500

    add category to food

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,5 +2,6 @@ class Food < ApplicationRecord
     validates :name, presence: true
     has_many :stories
     has_many :users, through: :stories
+    
     belongs_to :category
 end
--- db/migrate/20201201215423_add_category_id_to_food.rb
+++ db/migrate/20201201215423_add_category_id_to_food.rb
@@ -1,5 +1,6 @@
 class AddCategoryIdToFood < ActiveRecord::Migration[6.0]
   def change
     add_column :foods, :category_id, :integer
+    
   end
 end
